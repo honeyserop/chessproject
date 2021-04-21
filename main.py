1f2cd85cbb0aaa7fe8ae7e73b322847097ab97ae
honeyserop <77924262+honeyserop@users.noreply.github.com>
2021-04-21 23:19:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,89 +1,59 @@
-import pygame as p
-from chessgame import chessengine
+import tkinter as t
+import random as v
 
-width = height = 512
-dimension = 8
-square_size = height // dimension
-MAX_FPS = 15
-images = {}
+start_window = t.Tk()
+start_window.geometry("800x640")
+login = False
 
 
-def load_images():
-    pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "bp", "bR", "bN", "bB", "bQ", "bK"]
-    for piece in pieces:
-        images[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (square_size, square_size))
+def callmainwindow():
+    main_window = t.Toplevel(start_window)
+    main_window.geometry("800x640")
+    a = v.randrange(1, 999)
+    username = "guest" + str(a) + ""
+    if login == True:
+        user = t.Button(main_window, text=username, command=lambda: userinfopage(main_window)).place(x=400, y=0)
+    if login == False:
+        t.Button(main_window, text="log in or sign up", command=callloginpage).place(x=400, y=0)
 
 
-def main():
-    p.init()
-    screen = p.display.set_mode((width, height))
-    screen.fill(p.Color("blue"))
-    state = chessengine.game_state()
-    load_images()
-    running = True
-    valid_moves = state.get_checks()
-    move_made = False
-    sqr_selected = ()
-    player_clicks = []
-    while running:
-        for x in p.event.get():
-            if x.type == p.quit:
-                running = False
-            elif x.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()
-                column = location[0]//square_size
-                row = location[1] // square_size
-                if sqr_selected == (row, column):
-                    sqr_selected = ()
-                    player_clicks = []
-                else:
-                    sqr_selected = (row, column)
-                    player_clicks.append(sqr_selected)
-                    if len(player_clicks) == 2:
-                        move = chessengine.Movement(player_clicks[0], player_clicks[1], state.board)
-                        if move in valid_moves:
-                            state.makemove(move)
-                            move_made = True
-                            print(move.chessnotation())
-                            sqr_selected = ()
-                            player_clicks = []
-                        else:
-                            player_clicks = [sqr_selected]
-            elif x.type == p.KEYDOWN:
-                if x.key == p.K_z:
-                    state.undo_move()
-                    move_made = True
-
-        if move_made:
-            valid_moves = state.get_checks()
-            move_made = False
-        p.display.flip()
-        draw_board(state, screen)
+def userinfopage(main_window):
+    userinfo = t.Toplevel(main_window)
+    userinfo.geometry("800x640")
 
 
-def draw_board(state, screen):
-    draw_screen(screen)
-    draw_pieces(screen, state)
+def callloginpage():
+    global login_page
+    login_page = t.Toplevel(start_window)
+    login_page.geometry("300x250")
+    login_page.title("Account Login")
+    t.Label(login_page, text="Select Your Choice", bg="blue", width="300", height="2", font=("Calibri", 13))
+    t.Label(login_page, text="").pack()
+    tologin = t.Button(login_page, text="Login", height="2", width="30", command= logininterface).place(x=70, y=100)
+    t.Label(login_page, text="").pack()
+    toregister = t.Button(login_page, text="Register", height="2", width="30", command= registerinterface).place(x=70, y=200)
 
 
-def draw_screen(screen):
-    colors = (p.Color('white'), p.Color('grey'))
-    p.display.set_mode((width, height))
-    for r in range(dimension):
-        for c in range(dimension):
-            color = colors[((r + c) % 2)]
-            p.draw.rect(screen, color, p.Rect(c*square_size, r*square_size, square_size, square_size))
+def registerinterface():
+    registerscreen = t.Toplevel(login_page)
+    usernameentry = t.Entry(registerscreen)
+    usernameentry.pack()
+    passwordentry = t.Entry(registerscreen)
+    passwordentry.pack()
 
 
-def draw_pieces(screen, state):
-    for r in range(dimension):
-        for c in range(dimension):
-            lst = state.board
-            line = lst[r]
-            pieces = line[c]
-            if pieces != '--':
-                screen.blit(images[pieces], p.Rect(c*square_size, r*square_size, square_size, square_size))
+def logininterface():
+    loginscreen = t.Toplevel(login_page)
+    usernameentry = t.Entry(loginscreen)
+    usernameentry.pack()
+    passwordentry = t.Entry(loginscreen)
+    passwordentry.pack()
 
 
-if __name__ == '__main__':
-    main()
+titlemessage = t.Label(start_window, text="simplechess")
+titlemessage.place(x=395, y=50)
+guest_login = t.Button(start_window, text="login as guest", command=callmainwindow)
+guest_login.place(x=300, y=200)
+user_login = t.Button(start_window, text="register or log in", command=callloginpage)
+user_login.place(x=150, y=200 )
+start_window.mainloop()
